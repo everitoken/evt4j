01c0e7a83ed3995ca19d8826a0ba993e0eb0b3c9
Fei Liu <liu.feiwood@gmail.com>
2019-04-11 15:02:25 +0200

    bump up version

--- src/main/java/io/everitoken/sdk/java/apiResource/OkhttpApi.java
+++ src/main/java/io/everitoken/sdk/java/apiResource/OkhttpApi.java
@@ -56,7 +56,7 @@ class OkhttpApi {
     protected Request buildRequest(RequestParams requestParams) {
         RequestBody body = RequestBody.create(JSON_TYPE, requestParams.getApiParams().asBody());
 
-        return new Request.Builder().header(CUSTOM_REQUEST_HEADER, "1.0.0-rc2")
+        return new Request.Builder().header(CUSTOM_REQUEST_HEADER, "1.0.0-rc3")
                 .url(getUrl(requestParams.getNetParams())).post(body).build();
     }
 
